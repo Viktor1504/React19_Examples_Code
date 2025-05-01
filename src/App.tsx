e07c1f181df1086aa0cfad21106e25004e9374b1
victor <viktordenisevich6@gmail.com>
2025-05-02 00:14:15 +0300

    useOptimistic

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import {memo, useDeferredValue, useState} from "react"
+import { startTransition, useOptimistic, useState } from 'react'
 
-const List = memo(({filter}: { filter: string }) => {
-    const items = Array.from({length: 10000}, (_, i) => `Элемент ${i} ${filter}`).filter(item =>
-        item.toLowerCase().includes(filter.toLowerCase())
-    )
+function Counter() {
+  const [count, setCount] = useState<number>(0)
 
-    return (
-        <ul>
-            {items.map((item, index) => (
-                <li key={index}>{item}</li>
-            ))}
-        </ul>
-    );
-})
+  const [optimisticCount, addOptimistic] = useOptimistic(count)
 
+  const handleClick = () => {
+    startTransition(async () => {
+      try {
+        addOptimistic((prevValue) => prevValue + 1)
+        await new Promise<void>((resolve) => setTimeout(resolve, 1000))
+        setCount((prevCount) => prevCount + 1)
+      } catch (e) {
+        const error = e as Error
+        console.log(error)
+      }
+    })
+  }
 
-export default function App() {
-    const [input, setInput] = useState('')
-    const deferredFilter = useDeferredValue(input)
+  return (
+    <div>
+      <p>Оптимистичное значение: {optimisticCount}</p>
+      <p>Фактическое значение: {count}</p>
 
-    return (
-        <div>
-            <input
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                placeholder="Введите фильтр..."
-            />
-            <List filter={deferredFilter}/>
-        </div>
-    )
+      <button onClick={handleClick}>Увеличить</button>
+    </div>
+  )
 }
+
+const App = () => {
+  return <Counter />
+}
+
+export default App
