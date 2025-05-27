95d9d16ac990d001ee10f3a802dfd0962d351e33
victor <viktordenisevich6@gmail.com>
2025-05-28 00:17:53 +0300

    add useBeforeUnload

--- src/AddUserPage.tsx
+++ src/AddUserPage.tsx
@@ -1,7 +1,27 @@
+import { useCallback, useState } from 'react'
 import API from './api.ts'
 import { AxiosError } from 'axios'
+import { useBeforeUnload } from 'react-router'
 
 const AddUserPage = ({ refetchUsers }: { refetchUsers: () => void }) => {
+  const [hasUnsavedData, setHasUnsavedData] = useState(false)
+
+  // срабатывает перед выходом, обновлением страницы, закрытием вкладки
+  useBeforeUnload(
+    useCallback(
+      (e) => {
+        if (hasUnsavedData) {
+          e.preventDefault()
+        }
+      },
+      [hasUnsavedData],
+    ),
+  )
+
+  const handleInputChange = () => {
+    setHasUnsavedData(true)
+  }
+
   const handleAction = async (formData: FormData) => {
     const name = formData.get('name') as string
     const age = formData.get('age') as string
@@ -9,6 +29,7 @@ const AddUserPage = ({ refetchUsers }: { refetchUsers: () => void }) => {
     if (name && age) {
       try {
         await API.addUser({ id: crypto.randomUUID(), name, age })
+        setHasUnsavedData(false) // Сбрасываем флаг после успешного сохранения
         refetchUsers()
       } catch (error) {
         const err = error as AxiosError | Error
@@ -26,12 +47,14 @@ const AddUserPage = ({ refetchUsers }: { refetchUsers: () => void }) => {
         type="text"
         name="name"
         placeholder="Имя"
+        onChange={handleInputChange}
         className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors"
       />
       <input
         type="number"
         name="age"
         placeholder="Возраст"
+        onChange={handleInputChange}
         className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors"
       />
       <button
